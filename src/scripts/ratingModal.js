a72a56fa2e9a4e680217ae5a4af56a68bba3e382
Maxrrrrr <pavliv.maxym@gmail.com>
2024-01-29 22:21:53 +0200

    qw2qeqw

---
+++ src/scripts/ratingModal.js
@@ -0,0 +1,24 @@
+const contactModal = document.getElementById("contact_modal");
+const ratingButtons = document.querySelectorAll('.rating_button');
+const selectedRatingDiv = document.getElementById('rating_value');
+
+const toggleModal = () => {
+    contactModal.classList.toggle("active");
+}
+
+document.getElementById("contact_us").addEventListener('click', toggleModal);
+document.getElementById("close_modal").addEventListener('click', toggleModal);
+
+ratingButtons.forEach((button, index) => {
+    button.addEventListener('change', function () {
+        selectedRatingDiv.textContent =  button.value;
+        button.classList.add('active');
+        for (let i = 0; i < index; i++) {
+            ratingButtons[i].classList.add('active');
+        }
+        for (let i = index + 1; i < ratingButtons.length; i++) {
+            ratingButtons[i].classList.remove('active');
+        }
+    });
+});
+
